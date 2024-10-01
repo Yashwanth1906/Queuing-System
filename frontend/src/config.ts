0d0c0f668475b01854f950242efb8e774e807326
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2024-10-01 15:53:39 +0530

    production3-commit

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,2 @@
-// export const BACKEND_URL = "http://ec2-13-60-12-228.eu-north-1.compute.amazonaws.com:4000"
-export const BACKEND_URL = "http://localhost:4000"
+export const BACKEND_URL = "http://ec2-13-60-12-228.eu-north-1.compute.amazonaws.com:4000"
 export const HOSPITAL_CODE = "0001"
