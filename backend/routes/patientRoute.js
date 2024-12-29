e9b7dc006e965c0b0826bea05c2861e5504fa1b9
Yashwanth1906 <yashwanthsakthivel01@gmail.com>
2024-12-30 01:20:55 +0530

    intimate-remove

--- backend/routes/patientRoute.js
+++ backend/routes/patientRoute.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { bookBySymptoms, bookSlot, getBookedAppointments, getPatient, getSlots, intimations, patientLogin } from "../controllers/patientController.js";
+import { bookBySymptoms, bookSlot, getBookedAppointments, getPatient, getSlots, patientLogin } from "../controllers/patientController.js";
 
 import { authMiddleWare } from "../middleware/auth.js";
 import { getHospitalPrismaClient } from "../middleware/prismaProvider.js";
@@ -11,9 +11,6 @@ patientRouter.post("/login",patientLogin)
 patientRouter.get("/getdetails",authMiddleWare,getPatient)
 patientRouter.get("/appointments",authMiddleWare,getBookedAppointments)
 patientRouter.get("/gethospitals",authMiddleWare,getHospitals);
-patientRouter.post("/intimatereason",getHospitalPrismaClient,intimatebyreason);
 patientRouter.post("/getslots",getHospitalPrismaClient,authMiddleWare,getSlots);
-
 patientRouter.post("/bookslot",getHospitalPrismaClient,authMiddleWare,bookSlot);
 patientRouter.post("/bookwithsymptoms",getHospitalPrismaClient,authMiddleWare,bookBySymptoms);
-
